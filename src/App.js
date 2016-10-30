e39f009f00989457a5a4572ecc5051c2d07706b8
Tommasina <tommasina1@gmail.com>
2016-10-30 16:19:29 -0600

    Refines zip search to Hillary or Trump: - removes zipslist and fetchresults as components since storage is unnecessary - adds ability to render committee and total information based on zip code submission

--- src/App.js
+++ src/App.js
@@ -2,55 +2,72 @@ import React, { Component } from 'react';
 import './App.css';
 import InputSubmission from './InputSubmission';
 import ZipsList from './ZipsList';
-// import FetchResults from './FetchResults';
+import FetchResults from './FetchResults';
 import axios from 'axios';
 
 class App extends Component {
   constructor(){
     super();
     
-    // this.state = { zips: [] };
-    this.state = { zips: [], results: [] };
-  
+    this.state = { zip: '', response: [] };
 }
+
 componentDidMount() {
-  let zipsFromStorage = localStorage.getItem('zips');
-  zipsFromStorage = JSON.parse(zipsFromStorage);
+  // let zipsFromStorage = localStorage.getItem('zips');
+  // let resultsFromStorage = localStorage.getItem('results');
+  // zipsFromStorage = JSON.parse(zipsFromStorage);
+  // resultsFromStorage = JSON.parse(resultsFromStorage);
+  
 
-  this.setState({zips: zipsFromStorage ? zipsFromStorage : [], results: []});
-  axios.get('https://api.open.fec.gov/v1/committee/C00575795/schedules/schedule_a/by_zip/?api_key=nf4kDqBq4hjGmFR5gMmBc8XWjlwCWDFdHK9GoerZ&page=370')
-    .then(function(response){
-      console.log(response.data);
-      console.log(response.status);
-      this.setState({ results: response.data });
-    });
+  // this.setState({zips: zipsFromStorage ? zipsFromStorage : [], results:  resultsFromStorage ? resultsFromStorage : []});
 }
 
-addZip(zip) {
-    //here we will add an zip
-    this.state.zips.push( zip );
+// fetchByZip(zip){
+//   axios.get(`https://api.open.fec.gov/v1/schedules/schedule_a/by_zip/?api_key=nf4kDqBq4hjGmFR5gMmBc8XWjlwCWDFdHK9GoerZ&zip=${zip}&page=1&per_page=100&cycle=2016`)
+//     .then(res => {
+//       console.log(res.data.results);
+//       const response = res.data.results;
+//       this.setState({ response });
+//     });
+// }
 
-    //set state.zips to local storage
-    localStorage.setItem('zips', JSON.stringify(this.state.zips) );
-
-    this.setState( {zips: this.state.zips} );
-  }
-  deleteZip(id){
-      //make new array without zip
-      let newZipsList = this.state.zips.filter((zip) => zip.id !== id );
-      // change state of zips -> second argument is the callback to perform after setting state
-      this.setState( {zips: newZipsList},
-                        () => localStorage.setItem('zips', JSON.stringify(this.state.zips)) );
-    }
+ // results: 
+// addResult(result){
+//   this.state.results.push(result);
+//   localStorage.setItem('results', JSON.stringify(this.state.results) );
+//   this.setState( {results: this.state.results} );
+// }
+// 
+// addZip(zip) {
+//     this.state.zips.push( zip );
+//     localStorage.setItem('zips', JSON.stringify(this.state.zips) );
+//     this.setState( {zips: this.state.zips} );
+//   }
+//   deleteZip(id){
+//       let newZipsList = this.state.zips.filter((zip) => zip.id !== id );
+//       this.setState( {zips: newZipsList},
+//                         () => localStorage.setItem('zips', JSON.stringify(this.state.zips)) );
+//     }
 
     render() {
       return (
         <div id="main">
           <h1>FECensus</h1>
-          <InputSubmission sendZip={ this.addZip.bind(this) } />
-          <ZipsList zips={ this.state.zips } handleDeleteZip={ this.deleteZip.bind(this) } />
+          <InputSubmission/>
+
         </div>
       );
     }
   }
 export default App;
+// <FetchResults/>
+// response={ this.state.response }
+// <InputSubmission sendZip={ this.addZip.bind(this) } sendResult={ this.addResult.bind(this)} />
+// <ZipsList zips={ this.state.zips } handleDeleteZip={ this.deleteZip.bind(this) } />
+// <ul>
+// {this.state.response.map(oneResponse => 
+//   <li key={oneResponse.committee_id + oneResponse.total + oneResponse.count}>Total: {oneResponse.total}, State: {oneResponse.state_full}, Cycle: {oneResponse.cycle}</li>
+// )}
+// </ul>
+
+
--- src/InputSubmission.js
+++ src/InputSubmission.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class InputSubmission extends Component {
   constructor(){
     super();
     
-      this.state = { zip: ''};
+      this.state = { zip: '', response: ''};
   }
   
   handleClick(e) {
-    this.props.sendZip( Object.assign(this.state, {id: Date.now()}) );
-
-    //reset state
-    this.setState( {zip: ''} );
+    this.fetchByZip(this.state.zip); 
+    // this.props.sendZip( Object.assign(this.state.zip, {id: Date.now()}) );
+    // this.setState( {zip: '', result: ''} );
   }
+  
+  fetchByZip(zip){
+    axios.get(`https://api.open.fec.gov/v1/schedules/schedule_a/by_zip/?api_key=nf4kDqBq4hjGmFR5gMmBc8XWjlwCWDFdHK9GoerZ&zip=${zip}&page=1&per_page=100&cycle=2016`)
+      .then(res => {
+        console.log(res.data.results);
+        const response = res.data.results;
+        this.setState({ response });
+      });
+  }
+  
+  fetchTrump(){
+    let list = '';
+    this.state.response.map((item) => { 
+      if(item.committee_id === 'C00580100'){
+        list = <li key={item.committee_id + item.total + item.count}>Total: {item.total}, Committee ID: {item.committee_id} State: {item.state_full}, Cycle: {item.cycle}</li>
+      }
+    } ) 
+    return list
+  }
+  
+  fetchHillary(){
+    let list = '';
+    this.state.response.map((item) => { 
+      if(item.committee_id === 'C00575795'){
+        list = <li key={item.committee_id + item.total + item.count}>Total: {item.total}, Committee ID: {item.committee_id} State: {item.state_full}, Cycle: {item.cycle}</li>
+      }
+    } ) 
+    return list
+  }
+
   
   render() {
       return (
@@ -24,7 +54,11 @@ class InputSubmission extends Component {
             onChange={ ( event ) => this.setState({ zip: event.target.value }) }
              /><br/>
 
-          <button id="button" onClick={ this.handleClick.bind(this) } >Submit</button>
+          <button id="button" onClick={ this.handleClick.bind(this) }>Submit</button>
+          
+          <ul>
+          {this.state.response ? this.fetchTrump() : <li>"Please submit a zipcode to see the data"</li> }
+          </ul>
         </div>
       )
   }
