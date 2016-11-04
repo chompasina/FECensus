220359bd53a5d68e17644df7b7fcb4b00d5f0f2e
Tommasina <tommasina1@gmail.com>
2016-11-03 19:26:18 -0600

    Adds a few new endpoints for year to date, and all of the states totals

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import InputSubmission from './InputSubmission';
-import axios from 'axios';
+import YearToDate from './YearToDate';
+import States from './States';
+// import axios from 'axios';
 
 class App extends Component {
   constructor(){
     super();
     
-    this.state = { zip: '', response: [], hillaryList: '', trumpList: '' };
+    this.state = { zip: '', response: [], hillaryList: '', trumpList: ''};
 }
 
+
+  
 componentDidMount() {
 }
 
@@ -17,8 +21,9 @@ componentDidMount() {
       return (
         <div id="main">
           <h1>FECensus</h1>
+          <States/>
           <InputSubmission/>
-
+          <YearToDate/>
         </div>
       );
     }
---
+++ src/States.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class States extends Component {
+  constructor(){
+    super();
+    this.fetchTrump('P80001571');
+    this.fetchHillary('P00003392');
+    this.state = { clintonTotal: '', trumpTotal: ''};
+  }
+  
+  fetchHillary(candidate){
+    axios.get(`https://api.open.fec.gov/v1/schedules/schedule_a/by_state/by_candidate/?cycle=2016&page=1&api_key=nf4kDqBq4hjGmFR5gMmBc8XWjlwCWDFdHK9GoerZ&candidate_id=${candidate}&per_page=100&election_full=true`)
+      .then(res => {
+            var states = res.data.results;
+            this.setState({ clintonTotal: states }); 
+      });
+    };
+  fetchTrump(candidate){
+    axios.get(`https://api.open.fec.gov/v1/schedules/schedule_a/by_state/by_candidate/?cycle=2016&page=1&api_key=nf4kDqBq4hjGmFR5gMmBc8XWjlwCWDFdHK9GoerZ&candidate_id=${candidate}&per_page=100&election_full=true`)
+      .then(res => {
+            var states = res.data.results;
+            this.setState({ trumpTotal: states }); 
+      });
+    };
+  
+  
+  render(){
+    return (
+      <div className='states'  >
+      <ul>
+        <li> Clinton funds across the states: {this.state.clintonTotal ? this.state.clintonTotal.map(state => <p key={state.state}>State: {state.state_full} | Clinton Total: {state.total}</p>) : 'Everything broken' } </li>
+        <li> Trump funds across the states: {this.state.trumpTotal ? this.state.trumpTotal.map(state => <p key={state.state}>State: {state.state_full} | Trump Total: {state.total}</p>) : 'Everything broken' } </li>
+      </ul>
+      </div>
+    );
+  }
+}
+
+
+export default States;
